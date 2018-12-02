1b459c51363b5a0a24db1ef6a4d0e113af59662b
unknown <valentino_pezzano-github@yahoo.com>
2018-12-02 12:29:11 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# api-gateway
+This microservice works in conjunction with two other microservices, employee-service and spring-eureka-server.
+The Hystrix dashboard view is reachable at: http://localhost:8010/hystrix. The url to monitor in the dashboard is: 
+http://localhost:8010/actuator/hystrix.stream.
---
+++ src/main/java/com/example/howtodoinjava/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.example.howtodoinjava.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+ 
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class ApiGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
